3effa20fc54632c937ae05eabe8c7e8e905251da
walmirdev <walmir.neto@tnpromotora.com.br>
2025-10-21 13:48:24 -0300

    Adicionando botões

--- dio/src/App.js
+++ dio/src/App.js
@@ -1,14 +1,30 @@
 import Input from './components/input';
 import Button from './components/button';
-import { Container, Content } from './styles';
+import { Container, Content, row, Column, Row } from './styles';
 
 const App = () => {
   return (
     <Container>
       <Content>
-        Ola Mundo
         <Input />
-        <Button />
+        <Row>
+          <Button label="7"/>
+          <Button label="8"/>
+          <Button label="9"/>
+          <Button label="-"/>
+        </Row>
+        <Row>
+          <Button label="4"/>
+          <Button label="5"/>
+          <Button label="6"/>
+          <Button label="+"/>
+        </Row>
+        <Row>
+          <Button label="1"/>
+          <Button label="2"/>
+          <Button label="3"/>
+          <Button label="="/>
+        </Row>
       </Content>
     </Container>
   );
--- dio/src/components/button/styles.js
+++ dio/src/components/button/styles.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #000000ff;
+    background-color: #ff0000ff;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+`
