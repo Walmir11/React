0f8c341756d50718f2c0fe500c8fd289a9a3502d
Walmir Monteiro Neto <walmir.neto@lufratech.com.br>
2025-11-12 12:09:55 -0300

    Adicionando imagen background

--- dio/gitfind/src/pages/Home/index.js
+++ dio/gitfind/src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import { Header } from '../../components/Header';
+import background from '../../assets/background.png';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='conteudo'>
+        <img src={background} alt='background app' className='background' />
+
+      </div>
     </div>
   );
 }
