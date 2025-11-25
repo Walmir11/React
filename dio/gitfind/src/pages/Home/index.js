b2b6884bf837d8d1da6a109ef1fc272da4fe9420
Walmir Monteiro Neto <walmir.neto@lufratech.com.br>
2025-11-24 22:01:12 -0300

    Adicionando cabeçalho

--- dio/gitfind/src/pages/Home/index.js
+++ dio/gitfind/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header';
 import background from '../../assets/background.png';
+import './styles.css';
 
 function App() {
   return (
@@ -7,7 +8,21 @@ function App() {
       <Header />
       <div className='conteudo'>
         <img src={background} alt='background app' className='background' />
-
+        <div className='info'>
+          <div>
+            <input name='usuario' placeholder='@username' />
+            <button>Buscar</button>
+            <div className='perfil'>
+              <img src='https://avatars.githubusercontent.com/u/128555631?v=4' className='profile' alt='profile' />
+              <div>
+                <h3>Walmir Dev</h3>
+                <span>@walmirdev</span>
+                <p>Descrição do perfil</p>
+              </div>
+            </div>
+            <hr />
+          </div>
+        </div>
       </div>
     </div>
   );
