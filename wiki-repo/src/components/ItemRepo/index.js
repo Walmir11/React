d9302461e727cb828c970e699a0b659b52a23451
Walmir Monteiro Neto <walmir.neto@lufratech.com.br>
2025-12-08 15:26:59 -0300

    Finalizando wiki de repositórios

--- wiki-repo/src/components/ItemRepo/index.js
+++ wiki-repo/src/components/ItemRepo/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { ItemContainer } from "./styles";
 
-function ItemRepo() {
+function ItemRepo({repo, handleRemoveRepo}) {
+
+    const handleRemove = () => {
+        handleRemoveRepo(repo.id);
+    }
+
     return(
         <ItemContainer>
-            <h3>Item Title</h3>
-            <p>Item description goes here.</p>
-            <a href="#" >Ver repositório</a><br />
-            <a href="#" className="remover">Remove</a>
+            <h3>{repo.name}</h3>
+            <p>{repo.full_name}</p>
+            <a href={repo.html_url} rel="noreferrer" target="_blank" >Ver repositório</a><br />
+            <a href="#" rel="noreferrer" className="remover" onClick={handleRemove}>Remover</a>
             <hr />
         </ItemContainer>
     )
