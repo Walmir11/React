2f77c275815eda9f7cd1351f311dec0280c67c2a
Walmir Monteiro Neto <walmir.neto@lufratech.com.br>
2026-01-23 13:29:25 -0300

    Adicionando shema

--- dio/src/pages/register/index.jsx
+++ dio/src/pages/register/index.jsx
@@ -3,14 +3,32 @@ import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Container, Column, Title, Wrapper, TitleLogin, SubtitleLogin } from './styles';
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+const schema = yup
+  .object({
+    email: yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+    password: yup.string().min(3, 'Minimo 3 caracteres').required('Senha obrigatória'),
+    confirmPassword: yup.string()
+      .oneOf([yup.ref('password'), null], 'As senhas precisam ser iguais')
+      .required('Confirmar senha obrigatória'),
+  })
+  .required()
 
 const Register = () => {
-    const { control, handleSubmit, formState: { errors  } } = useForm({
-        reValidateMode: 'onChange',
-        mode: 'onChange',
-    });
+    const {
+    control,
+    handleSubmit,
+    formState: { errors},
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+  })
 
-    const onSubmit = async (formData) => {
+    const onSubmit = async formData => {
+        navigator('/login');
+
     };
 
   return (<>
@@ -24,10 +42,10 @@ const Register = () => {
         </Column>
         <Column>
             <Wrapper>
-                <TitleLogin>Faça seu login</TitleLogin>
+                <TitleLogin>Faça seu Cadastro</TitleLogin>
                 <SubtitleLogin>Informe seus dados para entrar na plataforma</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input placeholder="E-mail" name="email" control={control} />
+                    <Input type="email" placeholder="E-mail" name="email" control={control} />
                     <Input type="password" placeholder="Senha" name="password" control={control} />
                     <Input type="password" placeholder="Confirmar senha" name="confirmPassword" control={control} />
                     <Button title="Criar minha conta" variant="secondary" type="submit"/>
